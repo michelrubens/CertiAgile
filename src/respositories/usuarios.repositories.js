ac5fb82c956313d8204c8a9304ef2834ad952e0a
Michel Rubens <michelrube@gmail.com>
2026-05-01 17:29:07 -0300

    feat: enhance user creation flow with error handling and add exame insertion

--- src/respositories/usuarios.repositories.js
+++ src/respositories/usuarios.repositories.js
@@ -2,18 +2,23 @@ const pool = require('../database/db')
 const { randomBytes } = require('crypto')
 
 async function insertUsuario(client, nome, cpf, email, senha) {
-  const certificadoHash = randomBytes(24).toString('hex')
+  try {
+    const certificadoHash = randomBytes(24).toString('hex')
 
-  const result = await client.query(
-    `INSERT INTO usuarios (nome, cpf, email, senha, certificado_hash) 
+    const result = await client.query(
+      `INSERT INTO usuarios (nome, cpf, email, senha, certificado_hash) 
       VALUES ($1, $2, $3, $4, $5) 
       RETURNING id_usuario, nome, cpf, email, certificado_hash`,
-    [nome, cpf, email, senha, certificadoHash]
-  )
-  if (result && result.rowCount == 1) {
-    return result.rows[0]
+      [nome, cpf, email, senha, certificadoHash]
+    )
+    if (result && result.rowCount == 1) {
+      return result.rows[0]
+    }
+    return null
+  } catch (error) {
+    console.log(error)
+    return null
   }
-  return null
 }
 
 async function findFirstModuleId(client) {
@@ -42,6 +47,24 @@ async function findRandomGroup(client, moduloId) {
   return null
 }
 
+async function insertExame(client, moduloId, usuarioId, grupo, tentativa) {
+  try {
+    const result = await client.query(
+      `INSERT INTO exames (id_modulo, id_usuario, grupo, tentativa) 
+      VALUES ($1, $2, $3, $4) 
+      RETURNING id_exame`,
+      [moduloId, usuarioId, grupo, tentativa]
+    )
+    if (result && result.rowCount == 1) {
+      return result.rows[0]
+    }
+    return null
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
 async function createUsuario(nome, cpf, email, senha) {
   const client = await pool.connect()
   await client.query('BEGIN')
@@ -49,26 +72,38 @@ async function createUsuario(nome, cpf, email, senha) {
   const usuario = await insertUsuario(client, nome, cpf, email, senha)
   if (!usuario) {
     client.query('ROLLBACK')
-    return { message: 'Erro ao criar usuário' }
+    return { error: 'Erro ao criar usuário' }
   }
   const modulo = await findFirstModuleId(client)
   console.log('modulo: ', modulo)
   if (!modulo) {
     client.query('ROLLBACK')
-    return { message: 'Erro ao obter primeiro módulo' }
+    return { error: 'Erro ao obter primeiro módulo' }
   }
   const grupo = await findRandomGroup(client, modulo.id_modulo)
   if (!grupo) {
     client.query('ROLLBACK')
     console.log('modulo: ', modulo)
-    return { message: 'Erro ao obter grupo aleatório' }
+    return { error: 'Erro ao obter grupo aleatório' }
   }
+  const exame = await insertExame(
+    client,
+    modulo.id_modulo,
+    usuario.id_usuario,
+    grupo.grupo,
+    1
+  )
+  if (!exame) {
+    client.query('ROLLBACK')
+    return { error: 'Erro ao criar exame' }
+  }
+
   console.log('R: ', usuario.id_usuario, modulo.id_modulo, grupo.grupo)
 
   await client.query('COMMIT')
   client.release()
 
-  return usuario
+  return { message: 'Usuário criado com sucesso!' }
 }
 
 module.exports = {
